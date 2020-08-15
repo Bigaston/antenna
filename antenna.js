730b09d1fdeee266cc5488cc1980e86cb5102927
Bigaston <bigaston1@gmail.com>
2020-08-15 18:49:48 +0200

    🌟 Premier échanges de messages

--- antenna.js
+++ antenna.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const { ExpressPeerServer } = require('peer');
 const path = require("path")
 const fs = require("fs")
 
@@ -9,15 +10,39 @@ var app = express();
 app.get("/static/*", (req, res) => {
 	file_url = req.path.replace("/static/", "")
 
-	if (fs.existsSync("./static/" + file_url)) {
-		res.sendFile(path.join(__dirname, "./static/" + file_url))
+	if (fs.existsSync("./web/static/" + file_url)) {
+		res.sendFile(path.join(__dirname, "./web/static/" + file_url))
 	} else {
 		res.status(404).send("Not Found")
 	}
 });
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "/page/index.html"))
+	res.sendFile(path.join(__dirname, "/web/index.html"))
 })
 
-app.listen(process.env.PORT, () => console.log(`Serveur lancé sur le port ${process.env.PORT}`))
+const serv = app.listen(process.env.PORT, () => console.log(`Serveur lancé sur le port ${process.env.PORT}`));
+
+// Launch Peer Server
+const customGenerationFunction = () => {
+    let id = "";
+
+    for (i = 0; i < 3; i++) {
+        id = id + Math.round(Math.random()*9);
+    }
+
+    id = id + "-";
+
+    for (i = 0; i < 3; i++) {
+        id = id + Math.round(Math.random()*9);
+    }
+
+    return id;
+};
+
+const peerServer = ExpressPeerServer(serv, {
+    path: '/antenna',
+    generateClientId: customGenerationFunction
+});
+
+app.use('/peerjs', peerServer);
